19dbc221f6b6f4eab149387588dca5f794be2dca
geobalas <geobal87@yahoo.gr>
2012-12-10 18:03:14 +0200

    Add new file - test.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Nescafe.d</title>
+  </head>
+  <body>
+    <h1>Form</h1>
+	<form id="myform"action="next.php" method="get">
+		First Name: <input type="text" name="name"> <br>
+		Last Name: <input type="text" name="lastname">
+	</form>
+  </body>
+</html>
